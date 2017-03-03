3a178726c2313d4a54c3ca4e20d40a3948742a3a
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2017-03-02 18:27:17 -0600

    working picture of the day page

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var hbs = require('express-handlebars');
 var index = require('./routes/index');
 var users = require('./routes/users');
+require('./api_keys');
 
 var app = express();
 
 // view engine setup
+app.engine('.hbs', hbs({
+  extname: '.hbs',
+  defaultLayout: 'layout',
+  partialsDir : path.join(__dirname, 'views/partials')
+}));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var apod = require('../helpers/apod');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'AstroPix' });
 });
 
+router.get('/fetch_picture', function(req, res, next){
+  if(req.query.today){
+    apod(function(data, error){
+      if (error) {
+        return res.render('apod_error', { error : error.message });
+      }
+      return res.render('picture', { apod : data });
+    }, true);
+
+  } else if ( req.query.random){
+    apod(function(data, error) {
+      if (error) {
+        return res.render('apod_error', { error : error.message });
+      }
+      return res.render('picture', { apod : data });
+    });
+
+  } else{
+    next();
+  }
+});
 module.exports = router;
